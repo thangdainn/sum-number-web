f983e534004738024299bb011184636e023b3c9e
thann <thanngit@gmail.com>
2026-04-15 21:03:36 +0700

    Initial commit

---
+++ src/main/java/org/dainn/sumnumberweb/SumNumberWebApplication.java
@@ -0,0 +1,13 @@
+package org.dainn.sumnumberweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SumNumberWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SumNumberWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/dainn/sumnumberweb/SumNumberWebApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dainn.sumnumberweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SumNumberWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
